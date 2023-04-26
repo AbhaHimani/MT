f94872774b572438b5a563e9a23b39d9cbb77306
Abha Himani <74365827+AbhaHimani@users.noreply.github.com>
2023-04-27 02:06:24 +0530

    Update agarLcmChotaChahiye.m

--- agarLcmChotaChahiye.m
+++ agarLcmChotaChahiye.m
@@ -1,40 +1,52 @@
-cost = [20 19 14 21 16; 15 20 13 19 16; 18 15 18 20 10]
-supply = [40 60 70]
-sum(supply)
-demand=[30 40 50 40 60]
-sum(demand)
-[m,n]=size(cost);
-if sum(supply)==sum(demand)
-    disp("Balanced prblm");
-elseif sum(supply)<sum(demand)
-    disp("Unbalanced Problm");
-    cost(end+1,:)=zeros(1,n)
-    supply(end+1)=sum(demand)-sum(supply)
+clc;
+clear all;
+cost = [2 10 4 5; 6 12 8 11 ; 3 9 5 7];
+S = [12 25 20];
+D = [25 10 15 5];
+if sum(S)==sum(D)
+    fprintf('Balanced');
 else
-    disp("Unbalanced problem");
-    cost(:,end+1)=zeros(m,1)
-    demand(end+1)=sum(supply)-sum(demand)
+    fprintf('Unbalanced! Balancing it\n');
+    if sum(S)<sum(D)
+        cost(end+1,:) = zeros(1,size(D,2));
+        S(end+1)=sum(D)-sum(S);
+        mytable = [cost S';D sum(D)]  
+    else
+        cost(:,end+1) = zeros(size(S,2),1);
+        D(end+1)=sum(S)-sum(D);
+        mytable = [ cost S';D sum(S)]
+    end
 end
-disp("The balanced prblm is: ");
-balanced=['cost supply';demand sum(supply)]
-X=zeros(m,n);
-Icost=cost;
-while any(supply~=0)||any(demand~=0)
-    min_cost=min(cost(:))
-    [r,c]=find(cost==min_cost)
-    y=min(supply(r), demand(c))
-    [aloc, index]=max(y)
-    rr=r(index);
-    cc=c(index);
-    X(rr,cc)=aloc
-    supply(rr)=supply(rr)-aloc
-    demand(cc)=demand(cc)-aloc
-    cost(rr,cc)=Inf
+
+X = zeros(size(cost));
+
+cost1 = cost;
+
+[m,n] =size(cost);
+
+BVS = m + n - 1;
+
+for i=1:m*n
+
+    minim = min(cost(:));
+    [r,c] =find(minim ==cost);
+    x1 = min(S(r),D(c));
+
+    [value,index]= max(x1);
+    ii = r(index);
+    jj= c(index);
+    y = min( S(ii), D(jj) );
+
+    S(ii)= S(ii)-y;
+    D(jj)= D(jj) -y;
+    cost(ii,jj) = Inf;
+    X(ii,jj) = y;
 end
-if nnz(X)==m+n-1
-    disp("Non degenerate sol");
+X
+sum(sum(cost1.* X))
+
+if(length(nonzeros(X))==BVS)
+    fprintf('Non Degenerate')
 else 
-    disp("Degen sol");
+    fprintf('Degenerate')
 end
-final_cost=Icost.*X;
-Final_Cost=sum(final_cost(:))
