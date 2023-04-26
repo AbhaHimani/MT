d64faf46652b94d1f09a3727f8370eaec6bad8a0
Abha Himani <74365827+AbhaHimani@users.noreply.github.com>
2023-04-27 02:09:48 +0530

    Update BigM.m

--- BigM.m
+++ BigM.m
@@ -1,61 +1,51 @@
-*% variables={'x_1','x_2','x_3','s_1','s_2','s_3','sol'}
-% cost=[-1 3 -2 0 0 0 0];
-%info=[3 -1 2; -2 4 0; -4 3 8];
-%b=[7;12;10];
+clc
+clear all
 M=1000;
-A=[1 3 -1 0 1 0 3;1 1 0 -1 0 1 2];
-%b=[1;2];
-cost=[-3 -5 0 0 -M -M 0]
-% cost=[4 6 3 1 0 0 0 0];
-%info=[1 4 8 6;4 1 2 1;2 3 1 2];
-%b=[11;7;2];
-s=eye(size(A,1));
-%A=[info s b];
-% BV=[];
-% for j=1:size(s,2)
-%     for i=1:size(A,2)
-%         if A(:,i)==s(:,j)
-%             BV=[BV i];
-%         end
-%     end
-% end
-BV=[5 6];
-B=A(:,BV)
-A=inv(B)*A;
-ZjCj=cost(BV)*A-cost;
-Zcj=[ZjCj;A];
-SimplexTable=array2table(Zcj) 
+A=[5 1 -1 0 0 1 0 0;6 5 0 -1 0 0 1 0;1 4 0 0 -1 0 0 1];
+b=[10;30;8];
+c=[-12 -10 0 0 0 -M -M -M];
+c=[c 0];
+A=[A b];
+bv=[6 7 8];
+zjcj=c(bv)*A-c;
+simplex=[zjcj;A];
+var = {'x1','x2','s1','s2','s3','a1','a2','a3','sol'}
+array2table(simplex, 'VariableNames',var)
 RUN=true;
+n=length(zjcj);
 while RUN
-ZC=ZjCj(:,1:end-1);
-if any(ZC<0);
-    fprintf("NOt Optimal")
-    [EntVal,pvt_col]=min(ZC);
-    Sol=A(:,end);
-    Col=A(:,pvt_col);
-    if all(Col)<=0
-        fprintf("Unbounded")
+    if any(zjcj(1:n-1)<0)
+        fprintf('Solution is not optimal\n');
+        zc=zjcj(1:n-1);
+       [enter_var, pivot_col]=min(zc);
+       if all(A(:,pivot_col)<0)
+           error('LLP is unbounded');
+       else
+           sol=A(:,n);
+           col=A(:,pivot_col);
+           for i=1:size(A,1)
+               if col(i)>0
+                   ratio(i)=sol(i)/col(i);
+               else
+                   ratio(i)=inf;
+               end
+           end
+           [leaving_var,pivot_row]=min(ratio);
+       end
+       bv(pivot_row)=pivot_col;
+       pivot_key=A(pivot_row,pivot_col);
+       A(pivot_row,:)=A(pivot_row,:)/pivot_key;
+       for i=1:size(A,1)
+           if i~=pivot_row
+               A(i,:)=A(i,:)-A(i,pivot_col).*A(pivot_row,:)
+           end
+       end
+       zjcj=c(bv)*A-c;
+       simplex=[zjcj;A];
+       array2table(simplex, 'VariableNames',var)
     else
-    for i=1:size(A,1)
-        if Col(i)>0
-            ratio(i)=Sol(i)./Col(i);
-        else
-            ratio(i)=inf;
-        end
+        RUN=false;
     end
-    [minR,pvt_row]=min(ratio);
-    BV(pvt_row)=pvt_col;
-    B=A(:,BV)
-    end  
-A=B\A;
-ZjCj=cost(BV)*A-cost;
-
-else
-    RUN=false;
-    fprintf("Optimal")
 end
-end
-FINAL_BFS=zeros(1,size(A,2));
-FINAL_BFS(BV)=A(:,end);
-FINAL_BFS(end)=sum(FINAL_BFS.*cost)
-OptimalBFS=array2table(FINAL_BFS);*
+fprintf('Optimal Sol is %f\n',zjcj(end));
+%fprintf('In case of minimisation prob optimal Sol is %d\n',-zjcj(end));
