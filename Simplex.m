1354959f859e64c2f5bcfb0a843cb9b2b72bcdcf
Abha Himani <74365827+AbhaHimani@users.noreply.github.com>
2023-04-27 02:10:25 +0530

    Update Simplex.m

--- Simplex.m
+++ Simplex.m
@@ -1,51 +1,55 @@
-variables={'x_1','x_2','x_3','s_1','s_2','s_3','sol'}
-cost=[-1 3 -2 0 0 0 0];
-info=[3 -1 2; -2 4 0; -4 3 8];
-b=[7;12;10];
-s=eye(size(info,1));
-A=[info s b];
-% BV=[];
-% for j=1:size(s,2)
-%     for i=1:size(A,2)
-%         if A(:,i)==s(:,j)
-%             BV=[BV i];
-%         end
-%     end
-% end
-
-BV=[4 5 6];
-B=A(:,BV)
-A=inv(B)*A;
-ZjCj=cost(BV)*A-cost;
-Zcj=[ZjCj;A];
-SimplexTable=array2table(Zcj) 
+clc;
+clear all;
+a=[1 4 8 6;4 1 2 1;2 3 1 2];
+bv=[5 6 7];
+s=eye(size(a,1));
+a=[a s];
+sol=[11;7;2];
+cj=[4 6 3 1 0 0 0];
+cj=[cj 0];
+A=[a sol];
+var={'x1','x2','x3','x4','s1','s2','s3','sol'};
+zj_cj=cj(bv)*A-cj;
+firsttable=[zj_cj;A];
+array2table(firsttable,'VariableNames',var);
 RUN=true;
 while RUN
-ZC=ZjCj(:,1:end-1);
-if any(ZC<0);
-    fprintf("NOt Optimal")
-    [EntVal,pvt_col]=min(ZC);
-    Sol=A(:,end);
-    Col=A(:,pvt_col);
-    for i=1:size(A,1)
-        if Col(i)>0
-            ratio(i)=Sol(i)./Col(i);
+    if any(zj_cj<0)
+        fprintf('optimal soln not found\n');
+        z=zj_cj(1:end-1);
+        [enter,pivotcol]=min(z);
+        mycol=A(:,pivotcol);
+        if all(mycol<0)
+            error('Soln is unbounded');
         else
-            ratio(i)=inf;
+            sol=A(:,end);
+            for i=1:size(A,1)
+                if (mycol(i)>0)
+                    ratio(i)=sol(i)/mycol(i);
+                else
+                    ratio(i)=Inf;
+                end
+            end
+            [leave,pivotrow]=min(ratio);
+            pivotkey=A(pivotrow,pivotcol);
+            A(pivotrow,:)=A(pivotrow,:)/pivotkey;
+            for i=1:size(A,1)
+                if i~=pivotrow
+                    A(i,:)=A(i,:)-A(i,pivotcol)*A(pivotrow,:);
+                end
+            end
+            bv(pivotrow)=pivotcol;
+            zj_cj=cj(bv)*A-cj;
+            newtablearray=[zj_cj;A];
+            newtable=array2table(newtablearray,'VariableNames',var)
         end
+    else
+        RUN=false;
+        fprintf('Basic var indices');
+        bv
+        fprintf('Sol vector');
+        newtablearray(2:size(newtablearray,1),size(newtablearray,2))'
+        fprintf('optimal sol');
+        newtablearray(1,size(newtablearray,2))
     end
-    [minR,pvt_row]=min(ratio);
-    BV(pvt_row)=pvt_col;
-    B=A(:,BV)
-A=inv(B)*A;
-ZjCj=cost(BV)*A-cost;
-
-else
-    RUN=false;
-    fprintf("Optimal")
 end
-end
-FINAL_BFS=zeros(1,size(A,2));
-FINAL_BFS(BV)=A(:,end);
-FINAL_BFS(end)=sum(FINAL_BFS.*cost)
-OptimalBFS=array2table(FINAL_BFS);
